7253b4af20cb68c6debd45d95e8bf4d389ffbe0c
Sander Valstar <sandervalstar@gmail.com>
2021-04-06 19:45:05 -0700

    initial commit

---
+++ src/Math.java
@@ -0,0 +1,5 @@
+public class Math {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/MathTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MathTest {
+    @Test
+    public void testAdd() {
+        assertEquals(2, Math.add(1, 1));
+    }
+}
